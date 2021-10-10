26f4a7a8cdbba3d277a7dd75a3ec729195a7211e
Evgenyi <djonni-ussr@mail.ru>
2021-10-10 13:42:24 +0300

    feat: changed SecurityConfig

--- src/main/java/com/tasklist/auth/config/SecurityConfig.java
+++ src/main/java/com/tasklist/auth/config/SecurityConfig.java
@@ -6,13 +6,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity//(debug = true) //для вывода подробностей в лог
+@EnableWebSecurity(debug = true) //для вывода подробностей в лог
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable(); //Отключаем стандартную авторизацию Spring
-        http.formLogin().disable();
+        http.csrf().disable(); // отключаем на время разработки(для post put и др. которые изменяют данные)
+        http.httpBasic().disable(); //отключаем стандартную авторизацию Spring
+        http.formLogin().disable(); //отключаем стандартную форму логирования
         http.requiresChannel().anyRequest().requiresSecure(); //Обязательное исп. HTTPS
     }
 }
